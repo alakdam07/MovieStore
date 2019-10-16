bd84583499590badd5d45951c296c87596f171ec
alakdam07 <47625626+alakdam07@users.noreply.github.com>
2019-10-16 05:45:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Context from "./component/Context";
+import MovieList from "./component/MoviesList";
+import AddMovie from "./component/AddMovie";
+import NavBar from "./component/NavBar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context>
+      <div>
+        <NavBar />
+        <MovieList />
+        <AddMovie />
+      </div>
+    </Context>
   );
 }
-
-export default App;
---
+++ src/component/Movie.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import img from "./img/img.jpg";
+
+const useStyles = makeStyles(theme => ({
+  cardGrid: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2)
+  },
+  card: {
+    height: "100%"
+  },
+  cardMedia: {
+    paddingTop: "56.25%" // 16:9
+  }
+}));
+
+const Movie = ({ name, release, rating }) => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <div className={classes.root} style={{ background: "#111111" }}>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={1} item xs={6}>
+            <Grid>
+              <Card className={classes.card} item xs={6}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image={img}
+                  title="Image title"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Movie:{name}
+                  </Typography>
+                  <Typography>Movie realease date: {release}</Typography>
+                  <Typography>Movie rating: {rating}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Movie;
